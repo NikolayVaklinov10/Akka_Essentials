b885f05fe49dcd4dffe3f5d686735cf8f1ee85ce
Nick <nikolayvaklinov@yahoo.com>
2020-07-13 18:24:37 +0100

    child actor class and Parent actor class

--- src/main/scala/part2actors/ChildActors.scala
+++ src/main/scala/part2actors/ChildActors.scala
@@ -1,6 +1,40 @@
 package part2actors
 
+import akka.actor.{Actor, ActorRef, Props}
+
 object ChildActors extends App {
   // beginning of the child actor code
+
+  // companion object for parent
+  object Parent {
+    case class CreateChild(name: String)
+    case class TellChild(message: String)
+  }
+
+  class Parent extends Actor {
+    import Parent._
+
+
+    override def receive: Receive = {
+      case CreateChild(name) =>
+        println(s"${self.path} creating child")
+        // create a new actor right HERE
+        val childRef = context.actorOf(Props[Child], name)
+        context.become(withChild(childRef))
+
+    }
+
+    def withChild(childRef: ActorRef):Receive = {
+      case TellChild(message) =>
+        if (childRef != null) childRef forward message
+    }
+  }
+
+  class Child extends Actor {
+    override def receive: Receive = {
+      case message => println(s"${self.path} I got: $message")
+    }
+  }
+
 
 }

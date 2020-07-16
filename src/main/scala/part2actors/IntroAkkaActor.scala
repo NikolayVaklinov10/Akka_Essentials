2f87108005aef7a47c949eda04ecd0245e35a0bd
Nick <nikolayvaklinov@yahoo.com>
2020-07-16 16:15:17 +0100

    akka actor logging code

--- src/main/scala/part2actors/IntroAkkaActor.scala
+++ src/main/scala/part2actors/IntroAkkaActor.scala
@@ -1,6 +1,29 @@
 package part2actors
 
+import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
+import com.typesafe.config.ConfigFactory
+
 object IntroAkkaActor extends App {
 
+  class SimpleLoggingActor extends Actor with ActorLogging {
+    override def receive: Receive = {
+      case message => log.info(message.toString)
+    }
+  }
 
+  /**
+   * 1 - inline configuration
+   */
+  val configString =
+    """
+      | akka {
+      | loglevel = "DEBUG"
+      | }
+      |""".stripMargin
+
+  //
+  val config = ConfigFactory.parseString(configString)
+  val system = ActorSystem("ConfigurationDemo", ConfigFactory.load(config))
+  // instances
+  val actor = system.actorOf(Props[SimpleLoggingActor])
 }

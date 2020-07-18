dfca470dc808f21bb245b284a7662c91d2a0a686
Nick <nikolayvaklinov@yahoo.com>
2020-07-18 19:58:20 +0100

    my master actor

--- src/main/scala/part3testing/TestProbeSpec.scala
+++ src/main/scala/part3testing/TestProbeSpec.scala
@@ -1,5 +1,52 @@
 package part3testing
 
-class TestProbeSpec {
+import akka.actor.{Actor, ActorRef, ActorSystem}
+import akka.testkit.{ImplicitSender, TestKit}
+import jdk.nashorn.internal.ir.RuntimeNode.Request
+import org.scalatest.{BeforeAndAfterAll, WordSpecLike}
+
+class TestProbeSpec extends TestKit(ActorSystem("TestProbeSpec"))
+  with ImplicitSender
+  with WordSpecLike
+  with BeforeAndAfterAll {
+
+  override def afterAll(): Unit = {
+    TestKit.shutdownActorSystem(system)
+  }
+}
+
+// companion object
+object TestProbeSpec {
+  // scenario
+  /*
+    word counting actor hierarchy master-slave
+
+    send some work to the master
+      - master sends the slave the piece of work
+      - slave processes the work and replies to master
+      - master aggregates the result
+    master sends the total count to the original requester
+   */
+
+  case class Work(text: String)
+  case class SlaveWork(text: String, originalRequester: ActorRef)
+  case class WorkCompleted(count: Int, originalRequester: ActorRef)
+  case class Register(slaveRef: ActorRef)
+  case class Report(totalCount: Int)
+
+  class Master extends Actor {
+    override def receive: Receive = {
+      case Register(slaveRef) => context.become(online(slaveRef, 0))
+      case _ => // ignore
+    }
+
+    def online(slaveRef: ActorRef, totalWordCount: Int): Receive = {
+      case Work(text) => slaveRef ! SlaveWork(text, sender())
+      case WorkCompleted(count, originalRequester) =>
+        val newTotalWordCount = totalWordCount + count
+        originalRequester ! Report(newTotalWordCount)
+        context.become(online(slaveRef, newTotalWordCount))
+    }
+  }
 
 }

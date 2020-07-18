5be21c66b319406d47ea547c357030959f688e09
Nick <nikolayvaklinov@yahoo.com>
2020-07-19 00:18:09 +0100

    full code of the test probe file

--- src/main/scala/part3testing/TestProbeSpec.scala
+++ src/main/scala/part3testing/TestProbeSpec.scala
@@ -1,8 +1,7 @@
 package part3testing
 
-import akka.actor.{Actor, ActorRef, ActorSystem}
-import akka.testkit.{ImplicitSender, TestKit}
-import jdk.nashorn.internal.ir.RuntimeNode.Request
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+import akka.testkit.{ImplicitSender, TestKit, TestProbe}
 import org.scalatest.{BeforeAndAfterAll, WordSpecLike}
 
 class TestProbeSpec extends TestKit(ActorSystem("TestProbeSpec"))
@@ -13,14 +12,59 @@ class TestProbeSpec extends TestKit(ActorSystem("TestProbeSpec"))
   override def afterAll(): Unit = {
     TestKit.shutdownActorSystem(system)
   }
+
+  import TestProbeSpec._
+
+  "A master actor" should {
+    "register a slave" in {
+      val master = system.actorOf(Props[Master])
+      val slave = TestProbe("slave")
+
+      master ! Register(slave.ref)
+      expectMsg(RegistrationAck)
+    }
+
+    "send the work to the slave actor" in {
+      val master = system.actorOf(Props[Master])
+      val slave = TestProbe("slave")
+      master ! Register(slave.ref)
+      expectMsg(RegistrationAck)
+
+      val workloadString = "I love Akka"
+      master ! Work(workloadString)
+
+      // the interaction between the master and the slave actor
+      slave.expectMsg(SlaveWork(workloadString, testActor))
+      slave.reply(WorkCompleted(3, testActor))
+
+      expectMsg(Report(3)) // testActor receives the Report(3)
+    }
+
+    "aggregate data correctly" in {
+      val master = system.actorOf(Props[Master])
+      val slave = TestProbe("slave")
+      master ! Register(slave.ref)
+      expectMsg(RegistrationAck)
+
+      val workloadString = "I love Akka"
+      master ! Work(workloadString)
+      master ! Work(workloadString)
+
+      // in the meantime I don't have a slave actor
+      slave.receiveWhile() {
+        case SlaveWork(`workloadString`, `testActor`) => slave.reply(WorkCompleted(3, testActor))
+      }
+
+      expectMsg(Report(3))
+      expectMsg(Report(6))
+    }
+  }
 }
 
-// companion object
 object TestProbeSpec {
   // scenario
   /*
     word counting actor hierarchy master-slave
-
     send some work to the master
       - master sends the slave the piece of work
       - slave processes the work and replies to master
@@ -32,11 +76,14 @@ object TestProbeSpec {
   case class SlaveWork(text: String, originalRequester: ActorRef)
   case class WorkCompleted(count: Int, originalRequester: ActorRef)
   case class Register(slaveRef: ActorRef)
+  case object RegistrationAck
   case class Report(totalCount: Int)
 
   class Master extends Actor {
     override def receive: Receive = {
-      case Register(slaveRef) => context.become(online(slaveRef, 0))
+      case Register(slaveRef) =>
+        sender() ! RegistrationAck
+        context.become(online(slaveRef, 0))
       case _ => // ignore
     }
 
@@ -49,4 +96,5 @@ object TestProbeSpec {
     }
   }
 
+  // class Slave extends Actor ....
 }

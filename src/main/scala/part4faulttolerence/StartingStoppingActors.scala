ccaba67f8616dbeb2818ba35fced751723fa65d7
Nick <nikolayvaklinov@yahoo.com>
2020-07-22 17:22:45 +0100

    parent actor child actor and the parent actor's companion object

--- src/main/scala/part4faulttolerence/StartingStoppingActors.scala
+++ src/main/scala/part4faulttolerence/StartingStoppingActors.scala
@@ -1,5 +1,23 @@
 package part4faulttolerence
 
+import akka.actor.{Actor, ActorSystem}
+
 object StartingStoppingActors extends App {
+
+  val system = ActorSystem("StoppingActorsDemo")
+
+  object Parent {
+    case class StartChild(name: String)
+    case class StopChild(name: String)
+    case object Stop
+  }
+
+  class Parent extends Actor {
+    override def receive: Receive = ???
+  }
+
+  class Child extends Actor {
+    override def receive: Receive = ???
+  }
 
 }

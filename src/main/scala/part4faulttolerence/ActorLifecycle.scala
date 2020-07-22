9275cc27d1a606ea9853702bcc4383d53614603c
Nick <nikolayvaklinov@yahoo.com>
2020-07-22 18:53:54 +0100

    beginning of the lifecycle code commit

---
+++ src/main/scala/part4faulttolerence/ActorLifecycle.scala
@@ -0,0 +1,16 @@
+package part4faulttolerence
+
+import akka.actor.{Actor, ActorLogging, Props}
+
+object ActorLifecycle extends App {
+
+  object StartChild
+  class LifecycleActor extends Actor with ActorLogging {
+
+    override def receive: Receive = {
+      case StartChild =>
+        context.actorOf(Props[LifecycleActor], "child")
+    }
+  }
+
+}

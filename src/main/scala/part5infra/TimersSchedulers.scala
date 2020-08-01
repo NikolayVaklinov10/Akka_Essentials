0b269d8faf1e204b1688c2f47e1f23fc63be3247
Nick <nikolayvaklinov@yahoo.com>
2020-08-01 20:41:25 +0100

    the first scheduler

--- src/main/scala/part5infra/TimersSchedulers.scala
+++ src/main/scala/part5infra/TimersSchedulers.scala
@@ -1,5 +1,24 @@
 package part5infra
 
+import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
+import scala.concurrent.duration._
+
 object TimersSchedulers extends App {
+
+  class SimpleActor extends Actor with ActorLogging {
+    override def receive: Receive = {
+      case message => log.info(message.toString)
+    }
+  }
+  val system = ActorSystem("SchedulersTimersDemo")
+  val simpleActor = system.actorOf(Props[SimpleActor])
+
+  system.log.info("Scheduling reminder for simpleActor")
+  // the first scheduler
+  system.scheduler.scheduleOnce(1 second){
+    simpleActor ! "reminder"
+  }(system.dispatcher)
+
+
 
 }

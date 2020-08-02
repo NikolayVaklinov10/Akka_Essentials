00dd10e41e1e469ed25fad4e73d8267572ca315f
Nick <nikolayvaklinov@yahoo.com>
2020-08-02 11:26:29 +0100

    the way routers are constructed

--- src/main/scala/part5infra/Routers.scala
+++ src/main/scala/part5infra/Routers.scala
@@ -1,5 +1,42 @@
 package part5infra
 
+import akka.actor.{Actor, ActorLogging, Props, Terminated}
+import akka.routing.{ActorRefRoutee, RoundRobinRoutingLogic, Router}
+
 object Routers extends App {
+
+  /**
+   * #1 - manual router
+   */
+
+  class Master extends Actor {
+    // step 1 - create routees
+    // 5 actor routees based off Slave actors
+    private val slaves = for(_ <- 1 to 5) yield {
+      val slave = context.actorOf(Props[Slave])
+      context.watch(slave)
+      ActorRefRoutee(slave)
+    }
+
+    // step 2 - define router
+    private val router = Router(RoundRobinRoutingLogic(), slaves)
+
+    override def receive: Receive = {
+      // step 4 - handle the termination/lifecycle of the routees
+      case Terminated(ref) =>
+        router.removeRoutee(ref)
+        val newSlave = context.actorOf(Props[Slave])
+        context.watch(newSlave)
+        router.addRoutee(newSlave)
+        // step 3 - route the messages
+      case message =>
+        router.route(message, sender())
+    }
+  }
+  class Slave extends Actor with ActorLogging {
+    override def receive: Receive = {
+      case message => log.info(message.toString)
+    }
+  }
 
 }

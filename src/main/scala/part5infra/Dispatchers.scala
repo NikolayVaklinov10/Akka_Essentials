9f2abf08f71b51903b71e1bd3914d1ef5bd988b6
Nick <nikolayvaklinov@yahoo.com>
2020-08-02 19:06:20 +0100

    the class which is showing the dispatcher implementation from the config file

--- src/main/scala/part5infra/Dispatchers.scala
+++ src/main/scala/part5infra/Dispatchers.scala
@@ -1,6 +1,22 @@
 package part5infra
 
+import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
+import com.typesafe.config.ConfigFactory
+
 object Dispatchers extends App {
-  
+
+  class Counter extends Actor with ActorLogging {
+    var count = 0
+
+    override def receive: Receive = {
+      case message =>
+        count += 1
+        log.info(s"[$count] $message")
+    }
+  }
+
+  val system = ActorSystem("DispatcherDemo", ConfigFactory.load().getConfig("dispatchersDemo"))
+
+  val actors = for (i <- 1 to 10) yield system.actorOf(Props[Counter].withDispatcher("my-dispatcher"), s"counter_$i")
 
 }

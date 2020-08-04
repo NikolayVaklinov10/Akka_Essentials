ca11ac9a636aa3e148e6e58b6202594c9ee713f5
Nick <nikolayvaklinov@yahoo.com>
2020-08-04 13:31:27 +0100

    part 2 of the code

--- src/main/scala/part6patterns/FSMSpec.scala
+++ src/main/scala/part6patterns/FSMSpec.scala
@@ -168,3 +168,102 @@ object FSMSpec {
       self ! ReceiveMoneyTimeout
     }
   }
+
+  // step 1 - define the states and the data of the actor
+  trait VendingState
+  case object Idle extends VendingState
+  case object Operational extends VendingState
+  case object WaitForMoney extends VendingState
+
+  trait VendingData
+  case object Uninitialized extends VendingData
+  case class Initialized(inventory: Map[String, Int], prices: Map[String, Int]) extends VendingData
+  case class WaitForMoneyData(inventory: Map[String, Int], prices: Map[String, Int], product: String, money: Int, requester: ActorRef) extends VendingData
+
+  class VendingMachineFSM extends FSM[VendingState, VendingData] {
+    // we don't have a receive handler
+
+    // an EVENT(message, data)
+
+    /*
+      state, data
+      event => state and data can be changed.
+      state = Idle
+      data = Uninitialized
+      event(Initialize(Map(coke -> 10), Map(coke -> 1)))
+        =>
+        state = Operational
+        data = Initialized(Map(coke -> 10), Map(coke -> 1))
+      event(RequestProduct(coke))
+        =>
+        state = WaitForMoney
+        data = WaitForMoneyData(Map(coke -> 10), Map(coke -> 1), coke, 0, R)
+      event(ReceiveMoney(2))
+        =>
+        state = Operational
+        data = Initialized(Map(coke -> 9), Map(coke -> 1))
+     */
+
+    startWith(Idle, Uninitialized)
+
+    when(Idle) {
+      case Event(Initialize(inventory, prices), Uninitialized) =>
+        goto(Operational) using Initialized(inventory, prices)
+      // equivalent with context.become(operational(inventory, prices))
+      case _ =>
+        sender() ! VendingError("MachineNotInitialized")
+        stay()
+    }
+
+    when(Operational) {
+      case Event(RequestProduct(product), Initialized(inventory, prices)) =>
+        inventory.get(product) match {
+          case None | Some(0) =>
+            sender() ! VendingError("ProductNotAvailable")
+            stay()
+          case Some(_) =>
+            val price = prices(product)
+            sender() ! Instruction(s"Please insert $price dollars")
+            goto(WaitForMoney) using WaitForMoneyData(inventory, prices, product, 0, sender())
+        }
+    }
+
+    when(WaitForMoney, stateTimeout = 1 second) {
+      case Event(StateTimeout, WaitForMoneyData(inventory, prices, product, money, requester)) =>
+        requester ! VendingError("RequestTimedOut")
+        if (money > 0) requester ! GiveBackChange(money)
+        goto(Operational) using Initialized(inventory, prices)
+
+      case Event(ReceiveMoney(amount), WaitForMoneyData(inventory, prices, product, money, requester)) =>
+        val price = prices(product)
+        if (money + amount >= price) {
+          // user buys product
+          requester ! Deliver(product)
+          // deliver the change
+          if (money + amount - price > 0) requester ! GiveBackChange(money + amount - price)
+          // updating inventory
+          val newStock = inventory(product) - 1
+          val newInventory = inventory + (product -> newStock)
+
+          goto(Operational) using Initialized(newInventory, prices)
+        } else {
+          val remainingMoney = price - money - amount
+          requester ! Instruction(s"Please insert $remainingMoney dollars")
+
+          stay() using WaitForMoneyData(inventory, prices, product, money + amount, requester)
+        }
+    }
+
+    whenUnhandled {
+      case Event(_, _) =>
+        sender() ! VendingError("CommandNotFound")
+        stay()
+    }
+
+    onTransition {
+      case stateA -> stateB => log.info(s"Transitioning from $stateA to $stateB")
+    }
+
+    initialize()
+  }
+}
